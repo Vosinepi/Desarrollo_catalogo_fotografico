e90585779cffdb7deedfb3c7a2e5fa026612b3fe
Vosinepi <ismaelpiovani@gmail.com>
2022-06-24 23:22:57 -0300

    no ni

--- catalogo_fotos_app/admin.py
+++ catalogo_fotos_app/admin.py
@@ -1,3 +1,20 @@
 from django.contrib import admin
 
+from catalogo_fotos_app.models import *
+
+
 # Register your models here.
+
+
+class Users(admin.ModelAdmin):
+    list_display = ("nombre", "apellido", "mail", "user", "password")
+    search_fields = ("nombre", "apellido", "mail", "user")
+
+
+class Catalogo(admin.ModelAdmin):
+    list_display = ("nombre", "descripcion", "imagen", "precio")
+    search_fields = ("nombre", "descripcion", "imagen", "precio")
+
+
+admin.site.register(Catalogo, Catalogo)
+admin.site.register(Users, Users)
