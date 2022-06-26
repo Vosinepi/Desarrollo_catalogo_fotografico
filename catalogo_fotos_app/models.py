3a8b50c13183c9cc9fb7a1a781cb35acf107629c
Vosinepi <ismaelpiovani@gmail.com>
2022-06-26 12:16:06 -0300

    Estilo al album

--- catalogo_fotos_app/models.py
+++ catalogo_fotos_app/models.py
@@ -1,4 +1,8 @@
 from django.db import models
+import uuid
+from django.db import models
+from imagekit.models import ProcessedImageField
+from imagekit.processors import ResizeToFit, Transpose
 
 # Create your models here.
 
@@ -14,11 +18,41 @@ class Users(models.Model):
         return self.nombre
 
 
-class catalogo(models.Model):
-    nombre = models.CharField(max_length=50)
-    descripcion = models.CharField(max_length=50)
-    imagen = models.ImageField(upload_to="catalogo/")
-    precio = models.IntegerField()
+class Album(models.Model):
+    titulo = models.CharField(max_length=70)
+    descripcion = models.TextField(max_length=1024)
+    thumb = ProcessedImageField(
+        upload_to="albums",
+        processors=[Transpose(), ResizeToFit(300)],
+        format="JPEG",
+        options={"quality": 90},
+    )
+    tags = models.CharField(max_length=250)
+    is_visible = models.BooleanField(default=True)
+    creada = models.DateTimeField(auto_now_add=True)
+    modificada = models.DateTimeField(auto_now_add=True)
+    slug = models.SlugField(max_length=50, unique=True)
 
-    def __str__(self):
-        return self.nombre
+    def __unicode__(self):
+        return self.titulo
+
+
+class AlbumImage(models.Model):
+    image = ProcessedImageField(
+        upload_to="albums",
+        processors=[Transpose(), ResizeToFit(1280)],
+        format="JPEG",
+        options={"quality": 70},
+    )
+    thumb = ProcessedImageField(
+        upload_to="albums",
+        processors=[Transpose(), ResizeToFit(300)],
+        format="JPEG",
+        options={"quality": 80},
+    )
+    album = models.ForeignKey("album", on_delete=models.PROTECT)
+    alt = models.CharField(max_length=255, default=uuid.uuid4)
+    creada = models.DateTimeField(auto_now_add=True)
+    width = models.IntegerField(default=0)
+    height = models.IntegerField(default=0)
+    slug = models.SlugField(max_length=70, default=uuid.uuid4, editable=False)
