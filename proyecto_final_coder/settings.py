59195a8baf97df66ad7debc8c6a6b625590c43ef
Vosinepi <ismaelpiovani@gmail.com>
2022-07-05 22:50:43 -0300

    para heroku

--- proyecto_final_coder/settings.py
+++ proyecto_final_coder/settings.py
@@ -80,6 +80,7 @@ WSGI_APPLICATION = "proyecto_final_coder.wsgi.application"
 
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
+db_from_env = dj_database_url.config(conn_max_age=500)
 
 DATABASES = {
     "default": {
@@ -88,6 +89,7 @@ DATABASES = {
     }
 }
 
+DATABASES["default"].update(db_from_env)
 
 # Password validation
 # https://docs.djangoproject.com/en/4.0/ref/settings/#auth-password-validators
