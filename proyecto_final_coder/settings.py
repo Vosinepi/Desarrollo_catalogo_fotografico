3e14f07de0099616486133d12593c26d945954d2
Vosinepi <ismaelpiovani@gmail.com>
2022-07-06 00:00:02 -0300

    heroku

--- proyecto_final_coder/settings.py
+++ proyecto_final_coder/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = "django-insecure-w^k%q-(!6*lwd86uzs!j3#_)*0(=+$g7huu2*86ld-ca1r9g1m
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['fotospro.herokuapp.com']
 
 
 # Application definition
