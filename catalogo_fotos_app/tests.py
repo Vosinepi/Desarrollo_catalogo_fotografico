0f2373aaa2579aeb6b2c5d18416c177e2396af77
Vosinepi <ismaelpiovani@gmail.com>
2022-07-22 15:11:32 -0300

    1

--- catalogo_fotos_app/tests.py
+++ catalogo_fotos_app/tests.py
@@ -1,3 +1,21 @@
 from django.test import TestCase
 
+from .models import Contacto
+
 # Create your tests here.
+
+
+class ContactoTestCase(TestCase):
+    def setUp(self):
+        Contacto.objects.create(
+            nombre="Juan",
+            apellido="Perez",
+            email="jua@juan.com",
+            subject="Prueba",
+            menssage="Prueba",
+        )
+
+    def test_contacto_cread(self):
+        contacto = Contacto.objects.get(nombre="Juan")
+        self.assertEqual(contacto.nombre, "Juan")
+        self.assertEqual(contacto.apellido, "Perez")
